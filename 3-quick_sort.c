7567273b608c549efe47347fd954d3d7b6287c19
nasirisreal <nasirisreal@yahoo.com>
2023-04-25 14:29:03 -0700

    Adding a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -31,7 +31,7 @@ void swap_ints(int *a, int *b)
  */
 int lomuto_partition(int *array, size_t size, int left, int right)
 {
-	int *driver, up, down:
+	int *driver, up, down;
 
 	driver = array + right;
 	for (up = down = left; down < right; down++)
