d8d8b15ed4ef881a61c5c9e6884cae7005ba9cb5
nasirisreal <nasirisreal@yahoo.com>
2023-04-25 13:44:04 -0700

    Adding a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,6 @@
 #include "sort.h"
+#include <stddef.h>
+
 /**
  * quick_sort - function that sorts an array
  * of integers in ascending order using the
@@ -12,6 +14,7 @@ void quick_sort(int *array, size_t size)
 {
 	_qsort(array, 0, size - 1, size);
 }
+
 /**
  * _qsort - auxiliar function for the
  * quick_sort function
@@ -21,37 +24,49 @@ void quick_sort(int *array, size_t size)
  * @size: size of the array
  * Return: no return
  */
-void _qsort(int *a, int low, int high, int size)
+void _qsort(int *a, int low, int high, size_t size)
 {
-	int p, w, i;
+	int pivot, pivot_index, i;
 	int tmp;
 
 	if (low < high)
 	{
-		p = high;
-		w = low;
+		/* Select pivot element */
+		pivot = a[high];
+		pivot_index = low;
+
+		/* Partition the array around the pivot */
 		for (i = low; i < high; i++)
 		{
-			if (a[i] < a[p])
+			if (a[i] < pivot)
 			{
-				if (i != w)
+				if (i != pivot_index)
 				{
+					/* Swap elements */
 					tmp = a[i];
-					a[i] = a[w];
-					a[w] = tmp;
+					a[i] = a[pivot_index];
+					a[pivot_index] = tmp;
+
+					/* Print the array after each swap */
 					print_array(a, size);
 				}
-				w++;
+				pivot_index++;
 			}
 		}
-		if (w != p && a[w] != a[p])
+
+		/* Swap pivot with element at pivot index */
+		if (pivot_index != high && a[pivot_index] != a[high])
 		{
-			tmp = a[w];
-			a[w] = a[p];
-			a[p] = tmp;
+			tmp = a[pivot_index];
+			a[pivot_index] = a[high];
+			a[high] = tmp;
+
+			/* Print the array after each swap */
 			print_array(a, size);
 		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
+
+		/* Recursively sort sub-arrays */
+		_qsort(a, low, pivot_index - 1, size);
+		_qsort(a, pivot_index + 1, high, size);
 	}
 }
