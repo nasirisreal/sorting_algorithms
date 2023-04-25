d55d3eac7e82116dc1410ede9217d435e526f88f
nasirisreal <nasirisreal@yahoo.com>
2023-04-25 15:42:45 -0700

    Adding a function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+#include <stdbool.h>
 #include "sort.h"
 
 /**
@@ -8,21 +10,20 @@
  */
 void dll_adj_swap(listint_t **list, listint_t *left, listint_t *right)
 {
-	listint_t *swap;
+    if (left->prev != NULL)
+        left->prev->next = right;
+    else
+        *list = right;
 
-	if (left->prev)
-		left->prev->next = right;
-	else
-		*list = right;
-	if (right->next)
-		right->next->prev = left;
-	right->prev = left->prev;
-	left->prev = right;
-	swap = right;
-	left->next = right->next;
-	swap->next = left;
+    if (right->next != NULL)
+        right->next->prev = left;
 
-	print_list(*list);
+    right->prev = left->prev;
+    left->prev = right;
+    left->next = right->next;
+    right->next = left;
+
+    print_list(*list);
 }
 
 /**
@@ -32,42 +33,32 @@ void dll_adj_swap(listint_t **list, listint_t *left, listint_t *right)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	bool swapped_f, swapped_b;
-	int shake_range = 1000000, checks;
-	listint_t *temp;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	if (!list || !(*list) || !(*list)->next)
-		return;
+    bool swapped;
+    size_t size;
+    listint_t *current;
 
-	temp = *list;
-	do {
-		swapped_f = swapped_b = false;
-		for (checks = 0; temp->next && checks < shake_range; checks++)
-		{
-			if (temp->next->n < temp->n)
-			{
-				dll_adj_swap(list, temp, temp->next);
-				swapped_f = true;
-			}
-			else
-				temp = temp->next;
-		}
-		if (!temp->next)  /* first loop, measuring list */
-			shake_range = checks;
-		if (swapped_f)
-			temp = temp->prev;
-		shake_range--;
-		for (checks = 0; temp->prev && checks < shake_range; checks++)
-		{
-			if (temp->n < temp->prev->n)
-			{
-				dll_adj_swap(list, temp->prev, temp);
-				swapped_b = true;
-			}
-			else
-				temp = temp->prev;
-		}
-		if (swapped_b)
-			temp = temp->next;
-	} while (swapped_f || swapped_b);
+    do {
+        swapped = false;
+        for (current = *list; current->next != NULL; current = current->next) {
+            if (current->n > current->next->n) {
+                dll_adj_swap(list, current, current->next);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+
+        swapped = false;
+        for (current = current->prev, size = 0; current->prev != NULL; current = current->prev) {
+            if (current->n < current->prev->n) {
+                dll_adj_swap(list, current->prev, current);
+                swapped = true;
+            } else {
+                size++;
+            }
+        }
+    } while (swapped && size != 0);
 }
